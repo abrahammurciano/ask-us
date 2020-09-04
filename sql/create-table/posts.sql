dbf075be7039e7b5736ae01f209d80652777d8ef
Abraham Murciano <abrahammurciano@gmail.com>
2020-09-04 19:52:37 +0300

    posts, questions, answers sql and latex

---
+++ sql/create-table/posts.sql
@@ -0,0 +1,3 @@
+create table posts (
+	id number(8) generated always as identity primary key
+);
---
+++ sql/create-table/questions.sql
@@ -0,0 +1,7 @@
+create table questions(
+	post_id number(8) primary key references posts(id),
+	title varchar(255) not null,
+	body clob not null,
+	points number(10) default 0 not null,
+	timestamp date default sysdate not null
+);
