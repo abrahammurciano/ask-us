5b01b6953eef8ec2f3af79830388a095c8b68efb
Abraham Murciano <abrahammurciano@gmail.com>
2020-09-06 00:12:44 +0300

    added not null to foreign key

--- sql/create-table/comments.sql
+++ sql/create-table/comments.sql
@@ -1,6 +1,6 @@
 create table comments(
 	post_id number(8) primary key references posts(id),
-	parent_post_id number(8) references posts(id),
+	parent_post_id number(8) references posts(id) not null,
 	body clob not null,
 	points number(10) default 0 not null,
 	timestamp date default sysdate not null
