8340f2811b8ad266fa771cf286d0855f6ced4044
Abraham Murciano <abrahammurciano@gmail.com>
2020-09-06 00:04:03 +0300

    moved primary key after generated

--- sql/create-table/users.sql
+++ sql/create-table/users.sql
@@ -1,5 +1,5 @@
 create table users(
-	id number(8) primary key generated always as identity,
+	id number(8) generated always as identity primary key,
 	username varchar(20) unique not null,
 	email varchar(320) unique not null,
 	password char(44) not null,   -- base64 sha256 is 44 chars
