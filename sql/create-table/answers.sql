14549341064fcfcffc3d31fe3e074b997b86a887
Abraham Murciano <abrahammurciano@gmail.com>
2020-09-06 00:29:49 +0300

    made foreign key not null

--- sql/create-table/answers.sql
+++ sql/create-table/answers.sql
@@ -1,7 +1,7 @@
 create table answers(
 	post_id number(8) primary key references posts(id),
 	accepted number(1) default 0 not null,
-	question_id number(8) references questions(post_id),
+	question_id number(8) references questions(post_id) not null,
 	body clob not null,
 	points number(10) default 0 not null,
 	timestamp date default sysdate not null
