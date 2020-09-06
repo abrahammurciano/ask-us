cc40168c82b4694bf93baa9746c5cb05a7a1051b
Abraham Murciano <abrahammurciano@gmail.com>
2020-09-06 18:01:27 +0300

    made label column longer

--- sql/create-table/topics.sql
+++ sql/create-table/topics.sql
@@ -1,5 +1,5 @@
 create table topics(
 	id number(8) generated always as identity primary key,
-	label varchar(20) unique not null,
+	label varchar(255) unique not null,
 	description varchar(255) not null
 );
