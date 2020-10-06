2e1dc3560e91bc521eb15fd61ccebd79b41377a4
Abraham Murciano <abrahammurciano@gmail.com>
2020-10-07 00:25:26 +0300

    wrote newsletter query

--- sql/queries/newsletter.sql
+++ sql/queries/newsletter.sql
@@ -1,1 +1,15 @@
--- to do
+select * from (
+       select q.title, q.body question, a.body answer, a.points
+       from questions q
+       inner join answers a
+       on q.post_id = a.question_id
+       where a.post_id = (
+             select * from (
+                    select a2.post_id from answers a2
+                    where a2.question_id = q.post_id
+                    order by a2.points desc
+             ) where rownum = 1
+       )
+       and q.timestamp >= sysdate - 7
+       order by q.points desc
+) where rownum <= 15
