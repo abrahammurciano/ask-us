44793b7c8d1aa50d2117b7d818c5a45b95afb2bd
Abraham Murciano <abrahammurciano@gmail.com>
2020-10-08 19:50:20 +0300

    reorganised data

--- sql/rollback/rollback.sql
+++ sql/rollback/rollback.sql
@@ -1,16 +1,16 @@
--- Select the data
+-- 1. Select the data
 select * from users where username like 'a%';
 
--- Update email endings
+-- 2. Update email endings
 update users
 set email = substr(email, 1, instr(email, '@') - 1) || '@aol.com'
 where username like 'a%';
 
--- Select the updated data
+-- 3. Select the updated data
 select * from users where username like 'a%';
 
--- Undo the changes
+-- 4. Undo the changes
 rollback;
 
--- Select the rolled back data
+-- 5. Select the rolled back data
 select * from users where username like 'a%';
