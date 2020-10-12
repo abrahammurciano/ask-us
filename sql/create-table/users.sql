59f0724f2d70c3c9d6dc02950d9af92dd6d86b23
Abraham Murciano <abrahammurciano@gmail.com>
2020-10-12 23:26:41 +0300

    changed char to varchar

--- sql/create-table/users.sql
+++ sql/create-table/users.sql
@@ -2,6 +2,6 @@ create table users(
 	id number(8) generated always as identity primary key,
 	username varchar(20) unique not null,
 	email varchar(320) unique not null,
-	password char(44) not null,   -- base64 sha256 is 44 chars
+	password varchar(43) not null,   -- base64 sha256 is 44 chars
 	points number(10) default 0 not null
 );
--- sql/triggers/hash_password.sql
+++ sql/triggers/hash_password.sql
@@ -3,16 +3,16 @@ before insert or update
    on users
    for each row
 declare
-	hashed_pass char(44);
+	hashed_pass varchar(43);
 begin
-	select substr(
-			utl_raw.cast_to_varchar2(
-				utl_encode.base64_encode(
-					standard_hash(:new.password, 'SHA256'))
-			), 1, 43
-		)
-	into hashed_pass from dual;
 	if inserting or :old.password != :new.password then
-		:new.password := hashed_pass
+		select substr(
+				utl_raw.cast_to_varchar2(
+					utl_encode.base64_encode(
+						standard_hash(:new.password, 'SHA256'))
+				), 1, 43
+			)
+		into hashed_pass from dual;
+		:new.password := hashed_pass;
 	end if;
 end;
