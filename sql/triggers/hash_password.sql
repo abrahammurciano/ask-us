9d97962899b6a6ec0899ba71ec739f3796b3f93d
Abraham Murciano <abrahammurciano@gmail.com>
2020-10-12 22:49:43 +0300

    tried to fix hash func

--- sql/triggers/hash_password.sql
+++ sql/triggers/hash_password.sql
@@ -3,15 +3,16 @@ before insert or update
    on users
    for each row
 declare
-	hashed_pass varchar(40);
+	hashed_pass char(44);
 begin
-	select standard_hash(:new.password, 'SHA256')
+	select substr(
+			utl_raw.cast_to_varchar2(
+				utl_encode.base64_encode(
+					standard_hash(:new.password, 'SHA256'))
+			), 1, 43
+		)
 	into hashed_pass from dual;
 	if inserting or :old.password != :new.password then
-		:new.password := substr(
-			utl_raw.cast_to_varchar2(
-				utl_encode.base64_encode(hashed_pass)
-			), 1, 43
-		);
+		:new.password := hashed_pass
 	end if;
 end;
