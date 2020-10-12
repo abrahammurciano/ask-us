5ae72ae225464613802a7120da0d176e413e5603
Abraham Murciano <abrahammurciano@gmail.com>
2020-10-12 19:52:39 +0300

    started stage 3

---
+++ sql/triggers/hash_password.sql
@@ -0,0 +1,18 @@
+create trigger hash_password
+before insert or update
+   on users
+   for each row
+begin
+	if inserting or :old.password != :new.password then
+		:new.password := substr(
+			UTL_RAW.CAST_TO_VARCHAR2(
+				UTL_ENCODE.BASE64_ENCODE(
+					STANDARD_HASH(
+						:new.password
+						'SHA256'
+					)
+				)
+			), 1, 43
+		)
+	end if;
+end;
