85db942931b76e88fc924a23967121c8fbf782e8
Abraham Murciano <abrahammurciano@gmail.com>
2020-10-12 22:35:31 +0300

    workaround missing standard_hash

--- sql/triggers/hash_password.sql
+++ sql/triggers/hash_password.sql
@@ -2,16 +2,15 @@ create trigger hash_password
 before insert or update
    on users
    for each row
+declare
+	hashed_pass varchar(40);
 begin
+	select standard_hash(:new.password, 'SHA256')
+	into hashed_pass from dual;
 	if inserting or :old.password != :new.password then
 		:new.password := substr(
-			UTL_RAW.CAST_TO_VARCHAR2(
-				UTL_ENCODE.BASE64_ENCODE(
-					STANDARD_HASH(
-						:new.password,
-						'SHA256'
-					)
-				)
+			utl_raw.cast_to_varchar2(
+				utl_encode.base64_encode(hashed_pass)
 			), 1, 43
 		);
 	end if;
