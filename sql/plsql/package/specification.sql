250fd7d9c26b282b35585e651c0b25445c27e565
Abraham Murciano <abrahammurciano@gmail.com>
2020-10-13 22:09:22 +0300

    changed password type

--- sql/plsql/package/specification.sql
+++ sql/plsql/package/specification.sql
@@ -1,16 +1,16 @@
 create package user_management as
 	function hash_password(
-		password in varchar(255)
-	) return char(43);
+		password in users.password%type
+	) return users.password%type;
 
 	function check_password(
 		id in users.id%type,
-		password in varchar(255)
+		password in users.password%type
 	) return boolean;
 
 	procedure change_password(
 		id users.id%type,
-		old_pass varchar(255),
-		new_pass varchar(255)
+		old_pass users.password%type,
+		new_pass users.password%type
 	);
 end user_management;
