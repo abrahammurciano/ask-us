045eba0f2760a8f191d0c7f485ec683a047d8ed2
Abraham Murciano <abrahammurciano@gmail.com>
2020-10-13 20:17:00 +0300

    did change_password definition

--- sql/plsql/package/body.sql
+++ sql/plsql/package/body.sql
@@ -23,4 +23,35 @@ create package body user_management as
 	begin
 
 	end;
+
+	procedure change_password(
+		user_id users.id%type,
+		old_pass varchar(255),
+		new_pass varchar(255)
+	) is
+	begin
+		if check_password(user_id, old_pass) = false then
+			dbms_output.put_line('Error: Invalid password.');
+			return;
+		end if;
+
+		hashed_pass char(43) := hash_password(new_pass)
+
+		cursor c_users is
+			select username, password from users
+			where password = hashed_pass;
+		begin
+			for r_user in c_users
+			loop
+				dbms_output.put_line(
+					'Warning: User '
+					|| r_user.username
+					|| ' already has this password.'
+				);
+			end loop;
+		end;
+
+		update users set password = hashed_pass
+		where id = user_id;
+	end;
 ;
