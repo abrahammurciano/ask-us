14819dcc8381a119ce35a5142f7eac46ce726128
Abraham Murciano <abrahammurciano@gmail.com>
2020-10-13 23:49:28 +0300

    debugged package body

--- sql/plsql/package/body.sql
+++ sql/plsql/package/body.sql
@@ -1,6 +1,6 @@
 create package body user_management as
 	function hash_password(password in users.password%type)
-	return users.password%type is hashed_pass users.password%type
+	return users.password%type is hashed_pass users.password%type;
 	begin
 		select substr(
 			utl_raw.cast_to_varchar2(
@@ -17,43 +17,45 @@ create package body user_management as
 
 	function check_password(
 		id in users.id%type,
-		password in varchar(255)
-	)
-	return boolean is valid boolean
+		password in varchar
+	) return boolean is
+		valid number;
 	begin
 		select count(*) into valid
 		from users u
 		where (u.username = id)
 		and (hash_password(password) = u.password);
-		return valid;
+
+		if valid = 1 then
+			return true;
+		else
+			return false;
+		end if;
 	end;
 
 	procedure change_password(
 		id users.id%type,
-		old_pass varchar(255),
-		new_pass varchar(255)
+		old_pass varchar,
+		new_pass varchar
 	) is
-	declare
-		hashed_pass users.password%type := hash_password(new_pass)
+		hashed_pass users.password%type := hash_password(new_pass);
+		cursor c_users is
+			select username, password from users
+			where password = hashed_pass;
 	begin
 		if check_password(id, old_pass) = false then
 			dbms_output.put_line('Error: Invalid password.');
 			return;
 		end if;
 
-		cursor c_users is
-			select username, password from users
-			where password = hashed_pass;
-		begin
-			for r_user in c_users
-			loop
-				dbms_output.put_line(
-					'Warning: User '
-					|| r_user.username
-					|| ' already has this password.'
-				);
-			end loop;
-		end;
+		for r_user in c_users
+		loop
+			dbms_output.put_line(
+				'Warning: User '
+				|| r_user.username
+				|| ' already has this password.'
+			);
+		end loop;
 
 		update users u set password = hashed_pass
 		where u.id = id;
