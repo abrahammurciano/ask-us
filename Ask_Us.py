04bd61ad93dd05e3955a8642e90dca220ad65d7b
Ezra Dweck <edweck278@gmail.com>
2020-10-14 22:32:56 +0300

    Added database queries and

--- Ask_Us.py
+++ Ask_Us.py
@@ -2,7 +2,8 @@
 from PyInquirer import prompt
 import sqlite3
 from pprint import pprint
-connection = sqlite3.connect('AskUs.db')
+connection = sqlite3.connect('ask_us.db')
+user = None
 
 def enter_username():
     username_prompt = [
@@ -16,9 +17,9 @@ def enter_username():
 
 def valid_username(username):
     cursor = connection.cursor()
-    cursor.execute(f"SELECT username FROM users WHERE username = {username}")
-    user = cursor.fetchone()
-    if user:
+    cursor.execute("SELECT username FROM users WHERE username = ?", (username))
+    user_ = cursor.fetchone()
+    if user_:
         return True
     else:
         return False
@@ -35,106 +36,244 @@ def enter_password():
 
 def correct_password(username, password):
     cursor = connection.cursor()
-    cursor.execute(f"SELECT * FROM users WHERE username = {username} and password = {password}")
-    user = cursor.fetchone()
-    if user:
+    cursor.execute("SELECT * FROM users WHERE username = ? and password = ?", (username, password))
+    user_ = cursor.fetchone()
+    if user_:
         return True
     else:
         return False
 
-#fill in
-def filter_input(input):
-    # make sure keywords safe for sql
-    return input
+def get_user_data(username):
+    cursor = connection.cursor()
+    cursor.execute("select id, username from users where username = ?", (username))
+    user = cursor.fetchone()
 
-#fill in
-def get_your_topics(username):
-    # query database, return list of topics
-    return []
+def present_questions(questions):
+    titles = []
+    for question in questions:
+        titles.append(question[1])
+    qs = [
+        {
+            'type': 'list',
+            'name': 'question',
+            'message': 'Questions found, select one to continue:',
+            'choices': titles
+        }
+    ]
+    question = prompt(qs)
+    for i, q in enumerate(questions):
+        if q[1] in question:
+            break
+    display_question(questions[i])
 
-#fill in
+def display_question(question):
+    print("QUESTION:")
+    pprint(question)
+    cursor = connection.cursor()
+    cursor.execute("""
+        select body, points, timestamp
+        from comments
+        where parent_post_id = ?
+        order by points desc
+    """, (question[0]))
+    print("COMMENTS:")
+    pprint(cursor.fetchall())
+    cursor2 = connection.cursor()
+    cursor.execute("""
+        select body, points, accepted, timestamp
+        from answers
+        where question_id = ?
+        order by points desc
+    """, (question[0]))
+    print("ANSWERS:")
+    pprint(cursor2.fetchall())
+
 def get_questions_keyword():
     search = [
         {
             'type': 'input',
             'name': 'search',
             'message': 'What would you like to search for?',
-            'filter': filter_input
         }
     ]
     word = prompt(search)
     cursor = connection.cursor()
-    cursor.execute(f"SELECT * FROM questions WHERE lower(questions.title) like lower({word}) or lower(questions.body) like lower({word}) order by points desc;")
+    cursor.execute("""
+        select *
+        from questions
+        where lower(questions.title) like lower(?) or lower(questions.body) like lower(?) 
+        order by points desc
+        """, (word, word))
     questions = cursor.fetchall()
-    # what do we wanna do
-    pprint(questions)
+    present_questions(questions)
+
+
+def get_questions_topic():
+    cursor = connection.cursor()
+    cursor.execute("""
+        select * from (
+            select *
+            from questions q
+            where exists (
+                    select topic_id from follows
+                    where user_id = ?
+                    intersect
+                    select topic_id from relates_to
+                    where question_id = q.post_id
+                )
+            order by points desc
+        ) where rownum <= 10;
+        """, (user[0]))
+    questions = cursor.fetchall()
+    present_questions(questions)
 
 #fill in
-def get_questions_topic(username):
-    topics = get_your_topics(username)
-    topic_prompt = [
+def get_unanswered_questions():
+    cursor = connection.cursor()
+    cursor.execute("""
+            select * from (
+                select *
+                from questions q
+                where exists (
+                    select topic_id from follows
+                    where user_id = ?
+                    intersect
+                    select topic_id from relates_to
+                    where question_id = q.post_id
+                ) and not exists (
+                    select post_id from answers
+                    where question_id = q.post_id
+            )
+	        order by timestamp desc
+            ) where rownum <= 10;
+            """, (user[0]))
+    questions = cursor.fetchall()
+    present_questions(questions)
+
+#fill in
+def get_answers_to_you():
+    cursor = connection.cursor()
+    cursor.execute("""
+        select * from (
+            (
+                select
+                    q.title subject,
+                    to_char(substr(a.body, 1, 100)) preview,
+                    a.timestamp
+                from
+                    answers a
+                    inner join questions q
+                    on a.question_id = q.post_id
+                where
+                    q.author_id = ?
+            )
+            union
+            (
+                select
+                    q.title,
+                    to_char(substr(c.body, 1, 100)) preview,
+                    c.timestamp
+                from
+                    comments c
+                    inner join questions q
+                    on c.parent_post_id = q.post_id
+                where
+                    q.author_id = ?
+            )
+            union
+            (
+                select
+                    to_char(substr(a.body, 1, 20)) subject,
+                    to_char(substr(c.body, 1, 100)) preview,
+                    c.timestamp
+                from
+                    comments c
+                    inner join answers a
+                    on c.parent_post_id=a.post_id
+                where
+                    a.author_id = ?
+            )
+            union
+            (
+                select
+                    to_char(substr(p.body, 1, 20)) subject,
+                    to_char(substr(c.body, 1, 100)) preview,
+                    c.timestamp
+                from
+                    comments c
+                    inner join comments p
+                    on c.parent_post_id=p.post_id
+                where
+                    p.author_id = ?
+            )
+        ) order by timestamp desc;
+        """, (user[0], user[0], user[0], user[0]))
+    pprint(cursor.fetchall())
+
+
+def get_your_posts():
+    cursor = connection.cursor()
+    cursor.execute("""
+        select * from (
+	        select title, points, timestamp
+	        from questions
+	        where author_id = ?
+	        union
+	        select to_char(substr(body, 1, 4000)), points, timestamp
+	        from answers
+	        where author_id = ?
+	        union
+	        select to_char(substr(body, 1, 4000)), points, timestamp
+	        from comments
+	        where author_id = ?
+        ) order by timestamp desc;
+        """, (user[0], user[0], user[0]))
+    pprint(cursor.fetchall())
+
+def existing_topic(topic):
+    cursor = connection.cursor()
+    cursor.execute("select id from topics where label = ?", (topic))
+    topic_ = cursor.fetchone()
+    if topic_:
+        return True
+    else:
+        return False
+
+#fill in
+def ask_question():
+    # see if we can do checkbox of topics
+    questions = [
         {
-            'type': 'list',
-            'name': 'topics',
-            'message': 'What topic would you like to retrieve questions from?',
-            'choices': topics
+            'type': 'input',
+            'name': 'topic',
+            'message': 'What topic does your question relate to?'
+        },
+        {
+            'type': 'input',
+            'name': 'title',
+            'message': 'Enter a title:'
+        },
+        {
+            'type': 'input',
+            'name': 'body',
+            'message': 'What is your question?'
         }
     ]
-    topic = prompt(topic_prompt)
-    # query database...
+    answers = prompt(questions)
+    if not existing_topic(answers['topic']):
+        print("cannot use topic that doesnt exist")
+        return
+    cursor = connection.cursor()
+    cursor.execute("insert into posts default values")
+    cursor.execute("""
+    insert into questions(post_id, title, body, author_id)
+    values(?,?,?,?)
+    """, (cursor.lastrowid, answers['title'], answers['body'], user[0]))
+    cursor.execute("select id from topics where label = ?", (answers['topic']))
+    cursor.execute("insert into relates_to(question_id, topic_id) values(?,?)", (cursor.lastrowid, cursor.fetchone()))
 
-#fill in
-def get_unanswered_questions(username):
-    topics = get_your_topics(username)
-    topic_prompt = [
-        {
-            'type': 'list',
-            'name': 'topics',
-            'message': 'What topic would you like to retrieve questions from?',
-            'choices': topics
-        }
-    ]
-    topic = prompt(topic_prompt)
-    # query database...
 
-#fill in
-def get_answers_to_you(username):
-    # query database
-    print()
-
-#fill in
-def get_your_posts(username):
-    # query database
-    print()
-
-#fill in
-def get_topics():
-    # query database...
-    return []
-
-#fill in
-def follow_topics(username):
-    topics = get_topics()
-    def generate_choices(all_topics, my_topics):
-        choices = []
-        for topic in all_topics:
-            if topic in my_topics:
-                choices.append({'name':topic,'checked':True})
-            else:
-                choices.append(topic)
-    choices = generate_choices(topics, get_your_topics(username))
-    browse_topics = [
-        {
-            'type': 'checkbox',
-            'name': 'topics',
-            'message': 'Select which topics to follow:',
-            'choices': choices,
-        }
-    ]
-    answers = prompt(browse_topics)
-    # follow and unfollow in database
-
-def main_menu(username):
+def main_menu():
     menu = [
         {
             'type': 'list',
@@ -146,7 +285,7 @@ def main_menu(username):
                 '3- Retrieve the newest unanswered questions which relate to a topic you follow',
                 '4- Obtain all answers to questions as well as all comments to posts which were posted by you, sorted from newest to oldest',
                 '5- Retrieve all questions, answers, and comments you authored, sorted by newest first',
-                '6- Browse and follow new topics',
+                '6- Ask a question'
                 '0- Sign out'
             ]
         }
@@ -156,19 +295,19 @@ def main_menu(username):
     if command['command'][0] == '1':
         get_questions_keyword()
     elif command['command'][0] == '2':
-        get_questions_topic(username)
+        get_questions_topic()
     elif command['command'][0] == '3':
-        get_unanswered_questions(username)
+        get_unanswered_questions()
     elif command['command'][0] == '4':
-        get_answers_to_you(username)
+        get_answers_to_you()
     elif command['command'][0] == '5':
-        get_your_posts(username)
+        get_your_posts()
     elif command['command'][0] == '6':
-        follow_topics(username)
+        ask_question()
     else: # command['command'][0] == '0' exit...
         return
 
-    main_menu(username)
+    main_menu()
 
 if __name__ == "__main__":
     username = enter_username()
@@ -181,6 +320,8 @@ if __name__ == "__main__":
         print("incorrect password")
         password = enter_password()
 
-    main_menu(username)
+    get_user_data(username)
+
+    main_menu()
 
     connection.close()
